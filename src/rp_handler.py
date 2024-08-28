babf119ad270e4731992f31df65e41583b7c87b2
Muhammad Rehan Aslam <19831661+ranareehanaslam@users.noreply.github.com>
2024-08-29 04:02:45 +0500

    Update rp_handler.py

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -1,9 +1,8 @@
 import time
+
 import runpod
 import requests
 from requests.adapters import HTTPAdapter, Retry
-
-LOCAL_URL = "http://127.0.0.1:3000/sdapi/v1"
 
 automatic_session = requests.Session()
 retries = Retry(total=10, backoff_factor=0.1, status_forcelist=[502, 503, 504])
@@ -13,34 +12,65 @@ automatic_session.mount('http://', HTTPAdapter(max_retries=retries))
 # ---------------------------------------------------------------------------- #
 #                              Automatic Functions                             #
 # ---------------------------------------------------------------------------- #
-def wait_for_service(url):
-    """
-    Check if the service is ready to receive requests.
-    """
-    retries = 0
 
+def wait_for_service(url):
+    '''
+    Check if the service is ready to receive requests.
+    '''
     while True:
         try:
-            requests.get(url, timeout=120)
+            requests.get(url)
             return
         except requests.exceptions.RequestException:
-            retries += 1
-
-            # Only log every 15 retries so the logs don't get spammed
-            if retries % 15 == 0:
-                print("Service not ready yet. Retrying...")
+            print("Service not ready yet. Retrying...")
         except Exception as err:
             print("Error: ", err)
 
         time.sleep(0.2)
 
 
-def run_inference(inference_request):
-    """
-    Run inference on a request.
-    """
-    response = automatic_session.post(url=f'{LOCAL_URL}/txt2img',
-                                      json=inference_request, timeout=600)
+def run_inference(params):
+    config = {
+        "baseurl": "http://127.0.0.1:3000",
+        "api": {
+            "txt2img":  ("POST", "/sdapi/v1/txt2img"),
+            "img2img":  ("POST", "/sdapi/v1/img2img"),
+            "png-info": ("POST", "/sdapi/v1/png-info"),
+            "getModels": ("GET", "/sdapi/v1/sd-models"),
+            "getOptions": ("GET", "/sdapi/v1/options"),
+            "setOptions": ("POST", "/sdapi/v1/options"),
+            "getControlNetModels": ("GET", "/controlnet/model_list"),
+            "getControlNetModules": ("GET", "/controlnet/module_list"),
+            "getControlNetAPIVersion": ("GET", "/controlnet/version"),
+            "getControlNetDetect": ("POST", "/controlnet/detect"),
+        },
+        "timeout": 600
+    }
+
+    api_name = params["api_name"]
+    path = None
+
+    if api_name in config["api"]:
+        api_config = config["api"][api_name]
+    else:
+        raise Exception("Method '%s' not yet implemented" % api_name)
+
+    api_verb = api_config[0]
+    api_path = api_config[1]
+
+    response = {}
+
+    if api_verb == "GET":
+        response = automatic_session.get(
+                url='%s%s' % (config["baseurl"], api_path),
+                timeout=config["timeout"])
+
+    if api_verb == "POST":
+        response = automatic_session.post(
+                url='%s%s' % (config["baseurl"], api_path),
+                json=params, 
+                timeout=config["timeout"])
+
     return response.json()
 
 
@@ -48,9 +78,9 @@ def run_inference(inference_request):
 #                                RunPod Handler                                #
 # ---------------------------------------------------------------------------- #
 def handler(event):
-    """
+    '''
     This is the handler function that will be called by the serverless.
-    """
+    '''
 
     json = run_inference(event["input"])
 
@@ -59,6 +89,8 @@ def handler(event):
 
 
 if __name__ == "__main__":
-    wait_for_service(url=f'{LOCAL_URL}/sd-models')
-    print("WebUI API Service is ready. Starting RunPod Serverless...")
+    wait_for_service(url='http://127.0.0.1:3000/sdapi/v1/txt2img')
+
+    print("WebUI API Service is ready. Starting RunPod...")
+
     runpod.serverless.start({"handler": handler})
